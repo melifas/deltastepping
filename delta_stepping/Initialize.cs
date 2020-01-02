12be0a45691e8b978a94a6776d9459e335741e26
melifas <meletisfasoulis@yahoo.gr>
2020-01-02 21:16:35 +0200

    Add project files.

---
+++ delta_stepping/Initialize.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delta_stepping
+{
+    class Initialize
+    {
+        public const int delta = 3;
+
+        static List<int> distanceAr = new List<int>(4);
+
+        static List<int> bucketAr = new List<int>(getNumOfBuckets());
+
+        public static int getNumOfBuckets()
+        {
+            if ((distanceAr.Count / delta) % 1 == 0)//true is an integer
+            {
+                return distanceAr.Count / delta;
+            }
+            else
+            {
+                return (distanceAr.Count - 1) / delta;
+            }
+        }
+
+//----------------------------------initialize buckets-------------//
+       public void bucketrs()
+        {           
+            for (int i = 0; i < bucketAr.Count; i++)
+            {
+                bucketAr[i] = getNumOfBuckets() - 1;
+                bucketAr[0] = 0;
+
+            }
+        }
+//-----------------------------------------------------------------------//
+    }
+}
---
+++ delta_stepping/index.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delta_stepping
+{
+    class index
+    {
+        //--------------------------------------------------------------------------------//
+        public const int delta = 3;
+        public Nodes[] nodes = new Nodes[37];
+
+        public int getNumOfBuckets()
+        {
+            if ((nodes.Length / delta) % 1 == 0)//true is an integer
+            {
+                return nodes.Length / delta;
+            }
+            else
+            {
+                return (nodes.Length - 1) / delta;
+            }
+        }
+        //------------------------------------------------------------------------------//
+        int delta;
+        List<int> distanceArr;
+        List<bool> activeArr;        
+        List<int> weightArr;
+        List<int> fromArr;
+        List<int> toArr;
+        List<int> bucketArray;
+
+//------------------------------------------------------------------------------//
+        public bool checkIfAnyNodeIsAtive() 
+        {
+            for (int i = 0; i < activeArr.Count; i++)
+            {
+                if (activeArr[i]==true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+//------------------------------------------------------------------------------//
+        public List<int> searchForEdges(int nodeIndex)
+        {
+            List<int> edgesIndex = new List<int>();
+            for (int i = 0; i < edgesIndex.Count; i++)
+            {
+                if (nodeIndex == fromArr[i] || nodeIndex == toArr[i])
+                {
+                    edgesIndex.Add(i);
+                }
+            }
+            return edgesIndex;
+        }
+//-----------------------------------------------------------------------------------------//
+
+        public void ProcessBucket(int bucketNumber)
+        {
+            bool isTrue = true;
+            while (isTrue)
+            {
+                isTrue = false;
+                for (int i = 0; i < distanceArr.Count; i++)
+                {
+                    if (activeArr[i] && bucketArray[i] == bucketNumber)
+                    {
+                        isTrue = true;
+                        RelaxNode(i,searchForEdges(i));
+                    }
+                }
+            }
+        }
+//--------------------------------------------------------------------------------------------//
+
+        public void RelaxNode(int index, List<int> edges)
+        {
+            for (int i = 0; i < edges.Count; i++)
+            {
+                var j = edges[i];
+                var n = fromArr[j] == index ? toArr[j] : fromArr[j];
+                var newDistance = distanceArr[index] + weightArr[j];
+                if (newDistance < distanceArr[n])
+                {
+                    distanceArr[n] = newDistance;
+                    activeArr[n] = true;
+                    bucketArray[n] = distanceArr[n] / delta;
+                }
+                activeArr[index] = false;
+            }
+        }
+//---------------------------------------------------------------------------------------------------//
+    }
+}
